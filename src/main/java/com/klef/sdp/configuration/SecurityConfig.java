6efc309f37d9b97fafbf3c2ef38bded6d7e8b32e
KLU <2400032157@kluniversity.in>
2026-04-28 11:58:37 +0530

    Final CORS header fix

--- src/main/java/com/klef/sdp/configuration/SecurityConfig.java
+++ src/main/java/com/klef/sdp/configuration/SecurityConfig.java
@@ -73,9 +73,19 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("*"));
-        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedOrigins(List.of(
+        	    "http://localhost:2001",
+        	    "https://canvasvault.netlify.app"
+        	));
+
+        	config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+
+        	config.setAllowedHeaders(List.of(
+        	    "Authorization",
+        	    "Content-Type"
+        	));
+
+        	config.setAllowCredentials(true);
         
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
